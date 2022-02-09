a447f15a987df60e411296f46dcd6786899585f1
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-02-09 19:10:40 +0100

    bug corretto :)))

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','address','p_iva','cover_img',
     ];
 
     /**

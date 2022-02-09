bd13258c1034312f9785663980dd995a081a97ce
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-02-09 18:59:03 +0100

    bug address

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -52,7 +53,11 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'address' => ['required', 'string', 'max:255'],
+            'p_iva' => ['required', 'numeric', 'min:11'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+
+            /* da aggiungere categoria required */
         ]);
     }
 
@@ -64,10 +69,21 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+       
+        
+        $user = User::create([
+
             'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'address' => $data['address'],
+            'p_iva'=>$data['p_iva'],
+            'email'=>$data['email'],
+            'cover_img'=>Storage::put("img", $data['cover_img']), 
+            'password'=>Hash::make($data['password'])
+
         ]);
+
+        /* $user->categories */
+        
+        return $user;
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('address');
+            $table->string('p_iva');
+            $table->string('cover_img');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

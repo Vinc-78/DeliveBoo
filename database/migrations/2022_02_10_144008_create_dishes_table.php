cc5a01455170f22afd748e0ff40ff29684113cf6
Elea Luzi <85105911+Ele-nonsochi@users.noreply.github.com>
2022-02-10 16:20:49 +0100

    CreatE migration

---
+++ database/migrations/2022_02_10_144008_create_dishes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->string("name");
+            $table->string("price");
+            $table->boolean("visibility",true);
+            $table->text("description");
+            $table->longText("image_url");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}

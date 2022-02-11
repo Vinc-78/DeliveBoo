b5326c7c0e8c9322d60efff8c6b11be75fb7c20b
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-02-11 16:34:20 +0100

    adds col for the index page

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -3,18 +3,21 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <h1>questa è la index di Ordini</h1>
-        <ul class="list-group">
-            <li class="list-group-item">
-                @foreach ($orderList as $order)
+        <div class="col">
 
-                <a href="{{ asset("admin.orders.show", $order->id) }}">
-                    <h5>{{ $order->name_client . " " . $order->surname_client }}</h5>
-                </a>
-                <p class="small">{{ $order->phone_client }}</p>
-                @endforeach
-            </li>
-        </ul>
+            <h1>questa è la index di Ordini</h1>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    @foreach ($orderList as $order)
+                    
+                    <a href="{{ asset("admin.orders.show", $order->id) }}">
+                        <h5>{{ $order->name_client . " " . $order->surname_client }}</h5>
+                    </a>
+                    <p class="small">{{ $order->phone_client }}</p>
+                    @endforeach
+                </li>
+            </ul>
+        </div>
     </div>
 
 </div>

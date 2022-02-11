a3383ccba8dc90dc99c6e09a0a55f14d5b13cd4d
Elea Luzi <85105911+Ele-nonsochi@users.noreply.github.com>
2022-02-11 09:51:17 +0100

    Corretto Model

--- app/Dish.php
+++ app/Dish.php
@@ -11,13 +11,7 @@ class Dish extends Model
     protected $fillable = ["user_id", "name", "description", "price", "image_url", "visibility"];
 
 
-    public function users() {
-
-        return $this->belongsTo('App\User');
     
-    }
-
-
     public function orders() {
 
         return $this->belongsToMany('App\Order', 'dish_order');

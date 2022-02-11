649f4ebb937f8935337910a11ee5a09424489f64
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-02-11 16:30:16 +0100

    adds order list

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>questa è la index di Ordini</h1>
+<div class="container">
+    <div class="row">
+        <h1>questa è la index di Ordini</h1>
+        <ul class="list-group">
+            <li class="list-group-item">
+                @foreach ($orderList as $order)
+
+                <a href="{{ asset("admin.orders.show", $order->id) }}">
+                    <h5>{{ $order->name_client . " " . $order->surname_client }}</h5>
+                </a>
+                <p class="small">{{ $order->phone_client }}</p>
+                @endforeach
+            </li>
+        </ul>
+    </div>
+
+</div>
 @endsection

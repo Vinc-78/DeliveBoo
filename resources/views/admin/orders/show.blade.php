d3edcf16604af29aeb12fdf88254150c92dae585
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-02-11 16:50:00 +0100

    adds bootstrap container structure

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <h1>questa è lo show del singolo ordine</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
 @endsection

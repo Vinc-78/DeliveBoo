260830503d08cf458f0bf1a0a98a91c4307959cc
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-02-11 16:02:14 +0100

    iniziato CRUD Dishes

---
+++ resources/views/admin/dishes/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+
+        <h1> Qui vanno i piatti</h1>
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,6 @@ Route::middleware('auth')
 ->group( function() {
 
     Route::get('/', 'HomeController@index' )->name('home'); 
+
+    Route::resource("dishes", "DishController");
 });

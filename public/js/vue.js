e53863348cefccb06a35bccdaf80d95d76784f92
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-02-15 10:40:50 +0100

    vue router istallato

--- public/js/vue.js
+++ public/js/vue.js
@@ -12897,7 +12897,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\anto-\Desktop\Progetto Finale\DeliveBoo\resources\js\vue.js */"./resources/js/vue.js");
+module.exports = __webpack_require__(/*! C:\Laravel\DeliveBoo\resources\js\vue.js */"./resources/js/vue.js");
 
 
 /***/ })

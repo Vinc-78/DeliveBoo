4668c05ac3f1806f02edffa47c87eed1503cc800
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-02-15 12:07:38 +0100

    adds input form, email, password, confirm password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,16 +4,111 @@
 <div class="container">
     <div class="row justify-content-center">
         
-        <div class="row justify-content-center col-xl-10 col-lg-8 col-md-10 col-sm-12">
+        <div class="col-xl-10 col-lg-8 col-md-12 col-sm-12">
             <div class="col-12 mb-5 text-center">
                 <h1>{{ __('Register') }}</h1>
             </div>
 
-            <div class="">
+            <div class="col-12">
+                <h3>Crea il tuo account ristorante</h3>
+                <hr>
+            </div>
+
+            <div class="mx-4">
                 <form method="POST" action="{{ route('register') }}"
-                enctype="multipart/form-data">
+                enctype="multipart/form-data" class="text-center">
                     @csrf
+
+
                     {{-- Inizia inserimento dati  --}}
+
+                    {{-- NOME --}}
+                    <div class="row text-left mb-5">
+                        <label for="name" class="p-0 col-12"><h5>Inserisci il nome del ristorante</h5></label>
+
+                        <div class="col-5 px-0">
+
+                            <input type="text" id="name" name="name" placeholder="Inserisci nome dell'attività" class="form-control rounded-0 border  @error('name') is-invalid @enderror"
+                            value="{{ old("name") }}" required autocomplete="name" autofocus>
+                            
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    {{-- EMAIL --}}
+                    <div class="row text-left mb-5">
+                        
+                        <div class="col-12 p-0">
+
+                            <label for="email" class="p-0 col-12"><h5>Inserisci la e-mail del ristorante</h5></label>
+                            
+                            <div class="col-5 px-0">
+                                
+                                <input type="email" id="email" name="email" placeholder="Inserisci email dell'attività" class="form-control rounded-0 border  @error('email') is-invalid @enderror"
+                                value="{{ old("email") }}" required autocomplete="email">
+                                
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- PASSWORD --}}
+                    <div class="row text-left mb-5">
+                        
+                        {{-- password --}}
+                        <div class="col-6 p-0">
+
+                            <label for="password" class="p-0 col-12"><h5>Inserisci la password del profilo</h5></label>
+                            <div class="col-10 px-0">
+                                
+                                <input type="password" id="password" name="password" placeholder="Inserisci password" class="form-control rounded-0 border  @error('password') is-invalid @enderror"
+                                value="{{ old("password") }}" required autocomplete="password">
+                                
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <p class="small p-0 m-0">la password deve essere di almeno 6 caratteri</p>
+
+                        </div>
+
+                        {{-- conferma password --}}
+                        <div class="col-6 p-0">
+
+                            <label for="password-confirm" class="p-0 col-12"><h5>Coferma password</h5></label>
+                            <div class="col-10 px-0">
+                                
+                                <input type="password" id="password-confirm" name="pasword_confirmation" placeholder="Conferma password" class="form-control rounded-0 border  @error('password') is-invalid @enderror"
+                                value="{{ old("password") }}" required autocomplete="confirm-password">
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- <div class="form-group row">
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                    </div>  --}}
+
+
+
+
+
+
+
                     {{-- Nome --}}
                    {{--  <div class="form-group row">
                         <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
@@ -64,6 +159,10 @@
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
                     </div> --}}
+
+
+
+
                     {{-- address --}}
                     {{-- <div class="form-group row">
                         <label for="address" class="col-md-4 col-form-label text-md-right">Indirizzo</label>
@@ -79,7 +178,10 @@
                         </div>
                     </div> --}}
                     {{-- p_iva --}}
-                   {{--  <div class="form-group row">
+
+                    
+                  
+                    {{--  <div class="form-group row">
                         <label for="p_iva" class="col-md-4 col-form-label text-md-right">P.IVA</label>
 
                         <div class="col-md-6">
@@ -129,7 +231,7 @@
 
                     
 
-                {{-- versione con check box da sistemare grafica --}}
+                    {{-- versione con check box da sistemare grafica --}}
 
                     {{--  @foreach ($categories as $category)
                         <div class="form-check form-check-inline mb-4">
@@ -150,7 +252,7 @@
                     @endif --}}
 
 
-            {{-- Submit --}}                      
+                    {{-- Submit --}}                      
                     <div class="form-group row mb-0">
                         <div class="col-12 text-center">
                             <button type="submit" class="btn btn-primary">

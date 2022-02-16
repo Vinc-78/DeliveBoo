9046194ab18973d6655d71c1ed25a640f81afb30
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-02-16 09:53:39 +0100

    api e controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/users", "UserController@index");
+
+
+Route::get("/categories", "CategoryController@index");

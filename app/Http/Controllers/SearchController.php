122a788df8bb040e4e6065a89ba79f7450608b54
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-02-20 20:49:02 +0100

    add old query(to remaind)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,20 +13,7 @@ class SearchController extends Controller
     {
         public function search(Request $request) {
 
-            /* $filter = $request->params;
-
-            $countFilter =count($filter['filtri']);
-
-            $usersList = DB::table('users')
-            ->select(DB::raw('users.name, address, cover_img, users.id, users.slug, COUNT(users.id)'))
-            ->join('user_category', 'user_category.user_id', '=', 'user.id')
-            ->join('categories','categories.id','=','user_category.category_id')
-            ->whereIn('categories.name',$filter['filtri'])
-            ->groupBy('users.id')
-            ->havingRaw("count(users.id) = '$countFilter'");
-
-
-            return json_encode($usersList->get());  */
+           
 
             $filter = $request->params;
 
@@ -59,32 +46,25 @@ class SearchController extends Controller
                 return json_encode($usersList->get());
             }
 
-            //DB::enableQueryLog();
-            /* $usersList = User::whereHas('categories', function(Builder $query) use($filter){
-                $query
-                ->whereIn('categories.name', $filter)
-                ->groupBy('users.id')
-                ->having('count(users.id)', '=', count($filter));
-                
-              
-        
-                
-            }); */
-            //$queries = DB::getQueryLog();
-            //dd($queries);
+           
+            /* 
 
+            //Old Query 
+            
+            
+            $filter = $request->params;
+
+
+            $usersList = User::whereHas('categories', function(Builder $query) use($filter){
+                $query->where('categories.name', $filter);
+                
+            });
+
+    
+            return json_encode($usersList->get(['name', 'address', 'cover_img','slug'])); */
           
           
   
            
           }
 }
-/* $items = DB::table("items")
-            ->select("items.id","items.title"
-                ,"items.min_quantity"
-                ,DB::raw('SUM(items_count.quantity) as total_quantity'))
-            ->join("items_count","items_count.id_item","=","items.id")
-            ->groupBy("items.id")
-            ->having("total_quantity","<",DB::raw("items.min_quantity"))
-            ->get();
-print_r($items); */

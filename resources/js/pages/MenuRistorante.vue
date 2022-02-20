4e77d9bda652ff3ebab970100a2a4029f6d33675
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-02-20 12:41:37 +0100

    adds basic cart functionality

--- resources/js/pages/MenuRistorante.vue
+++ resources/js/pages/MenuRistorante.vue
@@ -1,58 +1,66 @@
 <template>
-        <div class="container">
-            <div v-if="this.MyMenu != null">
+  <div class="container">
+    <div v-if="this.myMenu != null">
+      <div class="text-center title-menu mb-5">
+        <!-- NOME DEL RISTORANTE -->
+        <h1 class="border-bottom border-dark pb-4">{{ myMenu.name }}</h1>
 
-                <div class="text-center title-menu mb-5 ">
+        <h3 class="pt-3">Menù</h3>
 
-                   <h1 class="border-bottom border-dark pb-4"> {{MyMenu.name}} </h1><!-- MyMenu è User -->  
-
-                    <!-- <h4>Del ristorante :</h4> -->
-
-                     <h3 class="pt-3">Menù</h3>
-
-                </div>
-
-                            
-                <div class="row row-cols-lg-4  row-cols-sm-2  row-cols-md-3  mt-mb-4" >
-
-                    <div class="col text-center mb-3" v-for="menu in MyMenu.dishes" :key="menu.id" >
-
-                        <div class="piatto"  v-if="menu.visibility">
-                            <!-- Lo stile dell'immagine funziona in line -->
-                            <img style="object-fit: cover; 
-                            
-                            height: 150px;
-                            " 
-                            
-                            :src="'/storage/' + menu.image_url" class="img-dish w-75" alt="">
-
-                            <div class="piatto">
-                                <p class="mt-3 mb-4 border-bottom border-dark font-weight-bold"> {{menu.name}}</p>
-                                
-                                <p style="height:80px" class="mb-3">{{menu.description}}</p>
-
-                            </div>
-
-                            <div>
-                                <p class="mb-4">{{menu.price}} € </p>
-                            </div>
-
-
-                        </div>
-
-                        
-
-                    </div>
-
-                     <!-- menu è il piatto -->
-                
-                </div>
-            </div>
-
-            <div v-else>
-                <h1>non ci sono menu al momento</h1>
-            </div>
+        <div>
+          <router-link to="/cart" class="btn btn-primary"
+            >Carrello {{ total }}</router-link
+          >
         </div>
+      </div>
+
+      <div class="row row-cols-lg-4 row-cols-sm-2 row-cols-md-3 mt-mb-4">
+        <!-- STAMPO TUTTI I PIATTI DEL RISTORANTE -->
+        <div
+          class="col text-center mb-3"
+          v-for="dish in myMenu.dishes"
+          :key="dish.id"
+        >
+          <div class="piatto" v-if="dish.visibility">
+            <!-- Lo stile dell'immagine funziona in line -->
+            <img
+              style="object-fit: cover; height: 150px"
+              :src="'/storage/' + dish.image_url"
+              class="img-dish w-75"
+              :alt="dish.name"
+            />
+
+            <div class="piatto">
+              <p class="mt-3 mb-4 border-bottom border-dark font-weight-bold">
+                {{ dish.name }}
+              </p>
+
+              <p style="height: 80px" class="mb-3">{{ dish.description }}</p>
+            </div>
+
+            <div>
+              <p class="mb-4">{{ dish.price }} €</p>
+            </div>
+
+            <div>
+              <button class="btn btn-success" @click="addToCart(dish)">
+                +
+              </button>
+              <button class="btn btn-warning" @click="removeToCart(dish)">
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <!-- menu è il piatto -->
+      </div>
+    </div>
+
+    <div v-else>
+      <h1>non ci sono menu al momento</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -60,16 +68,84 @@ export default {
     name: "MenuRistorante",
     data(){
         return {
-            MyMenu: []
+            myMenu: [],
+            total: 0
         }
     },
     mounted() {
-        // console.log(this.$route.params.id);
-        let slug = this.$route.params.slug
-        window.axios.get(`/api/menu/${slug}`).then((resp) => {
-            // console.log(resp.data.data/* .dishes */); 
-            this.MyMenu = resp.data.data
-        })
+        this.getData()  
+        this.getTotal()
+    },
+    methods: {
+        getData(){
+            // console.log(this.$route.params.id);
+            let slug = this.$route.params.slug
+            window.axios.get(`/api/menu/${slug}`).then((resp) => {
+                // console.log(resp.data.data/* .dishes */); 
+                this.myMenu = resp.data.data
+            })
+        },
+        addToCart(dish){
+            //se nel local storage non esiste il carrello
+            if(!localStorage.getItem("cart")){
+                //allora vado a creare la chiave cart e gli associo un array
+                localStorage.setItem("cart", JSON.stringify([]))
+            }
+            
+
+
+            //recupero la chiave cart dentro la funzione e gli associo l'array che si trova nel cart di local storage
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            //controllo se il piatto su cui ho cliccato è già presente nell'array 
+            const dishExists = cart.find((el) => el.product.id === dish.id)
+
+            //se il piatto è già presente nel cart  
+            if(dishExists){
+                //allora aumento la quantità
+                dishExists.qta++
+            }else{
+                //altrimenti pusho tutto il piatto con una "qta = 1"
+                cart.push(
+                    {
+                        qta: 1,
+                        product: dish
+                    }
+                )
+            }
+
+            localStorage.setItem("cart", JSON.stringify(cart))
+
+            this.total++
+        },
+        removeToCart(dish){
+            const cart = JSON.parse(localStorage.getItem("cart"));
+
+            if(cart.length !== 0){
+                const dishExists = cart.find((el) => el.product.id === dish.id)
+
+                if(dishExists && dishExists.qta > 1){
+                    dishExists.qta--
+                }else if(dishExists.qta === 1){
+                    //recupero l'id del piatto esistente per poi cancellarlo
+                    let dishRemove = cart.indexOf(dishExists);
+                    // console.log(dishRemove);
+                    cart.splice(dishRemove, 1)
+                }
+
+                localStorage.setItem("cart", JSON.stringify(cart))
+
+                this.total--
+            }
+        },
+        getTotal(){
+            const cart = JSON.parse(localStorage.getItem("cart"))
+            
+            if(cart.length !== 0 ){
+                cart.forEach(element => {
+                    this.total += element.qta
+                });
+            }
+        }
     }
 }
 
@@ -77,23 +153,15 @@ export default {
 </script>
 
 <style lang="scss" scope>
+.titleMenu {
+  font-family: cursive;
 
-.titleMenu{
-        font-family: cursive;
-        
-        h1{
-            font-weight: bold;
-            padding-bottom: 50px!important;
-        };
-        h4{
-            color: rgb(172, 170, 170);
-        }
-
-       
-      
-
-      
-
+  h1 {
+    font-weight: bold;
+    padding-bottom: 50px !important;
+  }
+  h4 {
+    color: rgb(172, 170, 170);
+  }
 }
-
 </style>

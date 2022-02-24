4ff40fa18ab7d05d7031fc27a1f250ae705ef02b
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-02-24 12:20:24 +0100

    charts task concluded

--- resources/views/admin/orders/chart.blade.php
+++ resources/views/admin/orders/chart.blade.php
@@ -5,47 +5,40 @@
         <div class="mb-3">
             <button class="btn btn-secondary"><a class="text-white" href={{ route('admin.home') }}>Torna alla dashboard</a></button>
         </div>
-        
-       
-            
             {{-- Inserisci i grafici --}}
             <div class="row mt-5">
-                <div class="col-xs-12 col-md-6 mb-3">
-                    <canvas id="myChart" width="200px" height="200px"></canvas>
+                <div class="col-md-8 col-xs-12 mb-3 mx-auto">
+                    <canvas id="myChart" width="240px" height="160px"></canvas>
                 </div>
 
-
-                <div class="col-xs-12 col-md-6">
-                    <canvas id="myChart2" width="200px" height="200px"></canvas>
-
-                </div>
             </div>
-
-          
-
-                {{-- Codice di prova per lo stile  --}}
-
-                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                
-                {{-- PROVA DEL TIO --}}
-                
-               
 
         <script>
            
             
             const data = {
                 labels: <?php echo json_encode($label_months) ?>,
-                datasets: [{
-                    label: 'Numero di Ordini per mese',
-                    backgroundColor: 'rgb(79, 44, 206)',
-                    borderColor: 'rgb(240, 234, 235)',
-                    data: <?php echo json_encode($chart_orders) ?>,
-                }]
+                datasets: [
+                    {
+                        type: 'line',
+                        label: 'Entrate per mese in €',
+                        backgroundColor: 'rgb(92, 221, 53)',
+                        borderColor: 'rgb(210, 221, 53)',
+                        data: <?php echo json_encode($chart_total) ?>,
+                        yAxisID: 'y2',
+                    },
+                    {
+                        type: 'bar',
+                        label: 'Numero di Ordini per mese',
+                        backgroundColor: 'rgb(79, 44, 206)',
+                        borderColor: 'rgb(240, 234, 235)',
+                        data: <?php echo json_encode($chart_orders) ?>,
+                    },
+                ]
             };
 
             const config = {
-            type: 'bar',
+            type: 'scatter',
             data: data,
                 options: {
                     responsive: true,
@@ -57,66 +50,38 @@
                             display: true,
                             
                         }
-                    }
-                }
-            }; 
-          
-            const datadue = {
-              labels: <?php echo json_encode($label_months) ?>,
-                datasets: [{
-                    label: 'Entrate per mese in €',
-                    backgroundColor: 'rgb(92, 221, 53)',
-                    borderColor: 'rgb(210, 221, 53)',
-                    data: <?php echo json_encode($chart_total) ?>,
-                }]
-            };
-          
-            const configdue = {
-                type: 'line',
-                data: datadue,
-                options: {
-                    responsive: true,
-                    plugins: {
-                        legend: {
-                            position: 'top',
+                    },
+                    scales: {
+                        y: {
+                            min: 0,
+                            max: 4
                         },
-                        title: {
-                            display: true,
-                            
+                        y2: {
+                            min: 0,
+                            max: 200
                         }
                     }
                 }
-            };
+            }; 
 
                 
-          </script>
+        </script>
 
 
-            <script>
+        <script>
 
-                const myChart = new Chart(
-                document.getElementById('myChart'),
-                config
-                );
+            const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+            );
 
-                const myChartdue = new Chart(
-                document.getElementById('myChart2'),
-                configdue
-                );
+            const myChartdue = new Chart(
+            document.getElementById('myChart2'),
+            configdue
+            );
+        
             
-               
-            </script>
-           
-           
-
-
-           
-
-
-
-
-           
-       
+        </script>
     </div>        
 
 
